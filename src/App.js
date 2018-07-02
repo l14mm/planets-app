12c34f3f64c85275dc1b2f5591ac2712b3ca0b47
liammoroney <liam.moroney14@gmail.com>
2018-07-02 18:52:20 +0100

    Half added form to add new planet.

--- src/App.js
+++ src/App.js
@@ -16,8 +16,24 @@ import IconButton from '@material-ui/core/IconButton';
 import PlanetCard from './components/PlanetCard';
 import CardActions from '@material-ui/core/CardActions';
 import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
+
+import Snackbar from '@material-ui/core/Snackbar';
+import CloseIcon from '@material-ui/icons/Close';
+import green from '@material-ui/core/colors/green';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import SnackbarContent from '@material-ui/core/SnackbarContent';
+
+import NumberFormat from "react-number-format";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+import TextField from "@material-ui/core/TextField";
+import FormControl from "@material-ui/core/FormControl";
+
+
 import style1 from './style1';
 import style2 from './style2';
+import { CardContent } from '@material-ui/core';
 
 const apiUrl = 'http://planetsapi20180630123940.azurewebsites.net/';
 
@@ -30,6 +46,34 @@ const styles = {
     backgroundColor: 'black',
     color: 'white',
   },
+  appPlanetCard: {
+    backgroundColor: 'red'
+  }
+};
+
+function NumberFormatCustom(props) {
+  const { inputRef, onChange, ...other } = props;
+
+  return (
+    <NumberFormat
+      {...other}
+      ref={inputRef}
+      onValueChange={values => {
+        onChange({
+          target: {
+            value: values.value,
+          },
+        });
+      }}
+      thousandSeparator
+      suffix=" km"
+    />
+  );
+}
+
+NumberFormatCustom.propTypes = {
+  inputRef: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
 };
 
 class App extends Component {
@@ -40,9 +84,13 @@ class App extends Component {
       viewMode: 1,
       selectedPlanet: null,
       planets: null,
+      isAddingPlanet: false,
+      numberformat: "1234",
+      open: true,
     };
     this.SelectPlanet.bind(this);
     this.ChangeView.bind(this);
+    this.RenderAddPlanetCard.bind(this);
     this.RetrievePlanets.bind(this);
     this.RetrievePlanets();
     this.SelectPlanet('earth');
@@ -111,9 +159,76 @@ class App extends Component {
   }
 
   RenderAddPlanetCard(classes) {
-    console.log("{this.RenderAddPlanetCard(classes)}");
-    return 
-    (<div style={{display:'block',fontSize:'20px'}}>Hello!</div>)
+    return <span style={{ padding: '5px' }}>
+    <Card type="button" style={{
+      backgroundColor:'white',
+      width:'300px',
+      height:'594px',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      }}>
+      {this.state.isAddingPlanet === false ? (
+      <div>
+        <CardContent>
+          <p>Add new planet</p>
+        </CardContent>
+          <Button onClick={() => {this.setState({isAddingPlanet: true})}} variant="fab" color="primary" aria-label="add" className={classes.button} style={{textAlign:'centre'}}>
+            <AddIcon />
+          </Button>
+        <CardActions style={{}}>
+        </CardActions>
+      </div>) : (
+        <div>
+          <TextField
+            id="with-placeholder"
+            label="Name"
+            className={classes.textField}
+            margin="normal"
+          />
+          <TextField
+            id="with-placeholder"
+            label="Distance from sun"
+            className={classes.textField}
+            margin="normal"
+          />
+          <TextField
+            id="with-placeholder"
+            label="Mass"
+            className={classes.textField}
+            margin="normal"
+          />
+          <TextField
+            className={classes.formControl}
+            label="Diameter"
+            value={this.state.numberformat}
+            //onChange={this.handleChange('numberformat')}
+            id="formatted-numberformat-input"
+            InputProps={{
+              inputComponent: NumberFormatCustom,
+            }}
+          />
+          <TextField
+            id="with-placeholder"
+            label="Description"
+            className={classes.textField}
+            margin="normal"
+          />
+          <div style={{paddingTop:'20px'}}>
+            <Button variant="contained" color="primary" className={classes.button}
+            style={{margin:'10px'}} onClick={()=>{this.setState({isAddingPlanet:false})}}>
+              Cancel
+            </Button>
+            <Button variant="contained" color="secondary" className={classes.button} style={{margin:'10px'}} onClick={()=>{this.setState({isAddingPlanet:false})}}
+            //disabled
+            >
+              Save
+            </Button>
+          </div>
+        </div>
+      )}
+    </Card>
+    </span>;
   }
 
   // Check if url is requesting to view a specific planet. e.g. /planet/earth
@@ -136,16 +251,50 @@ class App extends Component {
             </Typography>
           </Toolbar>
         </AppBar>
+        <Snackbar
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+          open={this.state.open}
+          autoHideDuration={6000}
+          // onClose={this.handleClose}
+        >
+    <SnackbarContent
+      //className={classNames(classes[variant], className)}
+      className={{backgroundColor: green[600]}}
+      aria-describedby="client-snackbar"
+      message={
+        <span id="client-snackbar" className="classnamemessage">
+          <CheckCircleIcon className="classnameicon" />
+          "my message"
+        </span>
+      }
+      // action={[
+      //   <IconButton
+      //     key="close"
+      //     aria-label="Close"
+      //     color="inherit"
+      //     className="close"
+      //     //onClick={onClose}
+      //   >
+      //     <CloseIcon className="classnameicon" />
+      //   </IconButton>,
+      // ]}
+    />
+    </Snackbar>
+
         <span style={{}}>
           <IconButton onClick={() => this.ChangeView(0)} aria-label="view-list">
             <ListIcon />
           </IconButton>
-              {this.RenderAddPlanetCard()}
+          <IconButton onClick={() => this.ChangeView(1)} aria-label="view-list">
+            <ListIcon />
+          </IconButton>
         </span>
         {this.state.viewMode === 0 ? (
           <div className="planets">
             <List component="nav" className="planets-list">
-              {this.RenderAddPlanetCard(classes)}
               {this.RenderPlanetList(classes)}
             </List>
             {this.state.selectedPlanet !== null ? (
@@ -168,7 +317,6 @@ class App extends Component {
             <span component="nav" className="planets-list" style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
               {this.RenderAddPlanetCard(classes)}
               {this.RenderPlanetList(classes)}
-              {this.RenderAddPlanetCard(classes)}
             </span>
           </div>
         )}
